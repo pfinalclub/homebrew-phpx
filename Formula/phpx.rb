d56658d95cf4e5dbc1de8982da19365a19a30d86
pfinal <liuxuzhu@smm.cn>
2026-02-05 16:49:11 +0800

    20260204 update

--- Formula/phpx.rb
+++ Formula/phpx.rb
@@ -1,38 +1,30 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://docs.brew.sh/rubydoc/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Phpx < Formula
-    desc "A npx-like tool for PHP - run PHP tools without installation"
-    homepage "https://github.com/pfinalclub/phpx"
-    # url "https://github.com/pfinalclub/phpx/releases/download/v0.1.0/phpx-macos-x86_64"
-    # sha256 "678ec61133181fdc2a680320876e7696b5e1ada242369fdc32a01538eac68407"
-    version "v0.1.0"
-    license ""
-  
+  desc "Run PHP tools without installation, like npx"
+  homepage "https://github.com/pfinalclub/phpx"
+  version "0.1.0"
+  license "MIT"
+
+  if Hardware::CPU.intel?
+    url "https://github.com/pfinalclub/phpx/releases/download/v0.1.0/phpx-macos-x86_64"
+    sha256 "dca45daeb618ef99fbea7242da00c5707fbf4b3877cf3d818b1a5b7ad1257f88"
+  elsif Hardware::CPU.arm?
+    url "https://github.com/pfinalclub/phpx/releases/download/v0.1.0/phpx-macos-aarch64"
+    sha256 "7e0904bbec28cedd611ad3e3480379cb23fc61b430d7cce436d1bd414014836a"
+  end
+
+  def install
+    # 关键：直接使用 `bin.install` 下载的文件名，它会自动复制到 bin 目录。
+    # 我们根据下载的文件名，将其重命名为通用的 “phpx”。
     if Hardware::CPU.intel?
-      url "https://github.com/pfinalclub/phpx/releases/download/v0.1.0/phpx-macos-x86_64"
-      sha256 "678ec61133181fdc2a680320876e7696b5e1ada242369fdc32a01538eac68407"
+      bin.install "phpx-macos-x86_64" => "phpx"
     elsif Hardware::CPU.arm?
-      url "https://github.com/pfinalclub/phpx/releases/download/v0.1.0/phpx-macos-aarch64"
-      sha256 "59c860496a99bf165021743a6ba1200a2d87c46f7ff253640de062a9196476ce"
-    end
-  
-    def install
-      bin.install "phpx-macos-x86_64" => "phpx" if Hardware::CPU.intel?
-      bin.install "phpx-macos-aarch64" => "phpx" if Hardware::CPU.arm?
-    end
-  
-    test do
-      # `test do` will create, run in and delete a temporary directory.
-      #
-      # This test will fail and we won't accept that! For Homebrew/homebrew-core
-      # this will need to be a test that verifies the functionality of the
-      # software. Run the test with `brew test phpx`. Options passed
-      # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-      #
-      # The installed folder is not in the path, so use the entire path to any
-      # executables being tested: `system bin/"program", "do", "something"`.
-      system "false"
+      bin.install "phpx-macos-aarch64" => "phpx"
     end
   end
-  
+
+  test do
+    # 这是一个真实的功能测试，验证安装是否成功以及基本功能。
+    # 运行 `phpx --version` 并检查其输出是否包含版本信息。
+    assert_match "phpx", shell_output("#{bin}/phpx --version")
+  end
+end
